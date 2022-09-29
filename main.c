ef0a93e5dc3fa914b284940c4d5a48f6d2452618
Guy Marshall <guymarshall9@gmail.com>
2022-09-29 22:08:37 +0100

    Add incomplete skeleton for national insurance calculations

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+    // Income Tax Band
+
     double income_tax_band_allowances_rate = 0.0;
     double income_tax_band_basic_rate = 0.2;
     double income_tax_band_higher_rate = 0.4;
@@ -36,7 +38,26 @@ int main(int argc, char const *argv[])
 
     int income_tax_band_total_payable = income_tax_band_allowances_tax_payable_in_band + income_tax_band_basic_tax_payable_in_band + income_tax_band_higher_tax_payable_in_band + income_tax_band_above_tax_payable_in_band;
 
-    
+    // National Insurance Tax Band
+
+    double national_insurance_tax_band_primary_threshold_rate = 0.0;
+    double national_insurance_tax_band_upper_earnings_limit_rate = 0.1325;
+    double national_insurance_tax_band_above_rate = 0.0325;
+
+    int national_insurance_tax_band_primary_threshold_width = 104800;
+    int national_insurance_tax_band_upper_earnings_limit_width = 418900;
+
+    int national_insurance_tax_band_allowances_subject_to_tax_in_band = ;
+    int national_insurance_tax_band_basic_subject_to_tax_in_band = ;
+    int national_insurance_tax_band_higher_subject_to_tax_in_band = ;
+    int national_insurance_tax_band_above_subject_to_tax_in_band = ;
+
+    int national_insurance_tax_band_allowances_tax_payable_in_band = ;
+    int national_insurance_tax_band_basic_tax_payable_in_band = ;
+    int national_insurance_tax_band_higher_tax_payable_in_band = ;
+    int national_insurance_tax_band_above_tax_payable_in_band = ;
+
+    int national_insurance_tax_band_total_payable = ;
 
     return 0;
 }
