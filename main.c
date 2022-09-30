bb9b57b5b08311f8b5787f7de63678a2d7c41fc3
Guy Marshall <guymarshall9@gmail.com>
2022-09-30 19:12:37 +0100

    Change to pounds

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
     scanf("%d", &monthly_salary);
 
     const int PERIODS_IN_YEAR = 12;
-    const int PERSONAL_ALLOWANCE = 1257000;
+    const int PERSONAL_ALLOWANCE = 12570;
 
     if (monthly_salary < 0)
     {
@@ -32,21 +32,21 @@ int main(int argc, char const *argv[])
     double income_higher_rate = 0.4;
     double income_above_rate = 0.5;
 
-    int income_allowances_ceiling = PERSONAL_ALLOWANCE / PERIODS_IN_YEAR;
-    int income_basic_ceiling = (5027000-PERSONAL_ALLOWANCE) / PERIODS_IN_YEAR;
-    int income_higher_ceiling = (15000000 - 5027100) / PERIODS_IN_YEAR;
+    double income_allowances_ceiling = PERSONAL_ALLOWANCE / PERIODS_IN_YEAR;
+    double income_basic_ceiling = (50270-PERSONAL_ALLOWANCE) / PERIODS_IN_YEAR;
+    double income_higher_ceiling = (150000 - 50271) / PERIODS_IN_YEAR;
 
-    int income_allowances_subject_to_tax = min(income_allowances_ceiling, monthly_salary);
-    int income_basic_subject_to_tax = min(income_basic_ceiling, (monthly_salary - income_allowances_subject_to_tax));
-    int income_higher_subject_to_tax = min(income_higher_ceiling, (monthly_salary - income_allowances_subject_to_tax - income_basic_subject_to_tax));
-    int income_above_subject_to_tax = max(0, monthly_salary - income_allowances_subject_to_tax - income_basic_subject_to_tax - income_higher_subject_to_tax);
+    double income_allowances_subject_to_tax = min(income_allowances_ceiling, monthly_salary);
+    double income_basic_subject_to_tax = min(income_basic_ceiling, (monthly_salary - income_allowances_subject_to_tax));
+    double income_higher_subject_to_tax = min(income_higher_ceiling, (monthly_salary - income_allowances_subject_to_tax - income_basic_subject_to_tax));
+    double income_above_subject_to_tax = max(0, monthly_salary - income_allowances_subject_to_tax - income_basic_subject_to_tax - income_higher_subject_to_tax);
 
-    int income_allowances_tax_payable = income_allowances_rate * income_allowances_subject_to_tax;
-    int income_basic_tax_payable = income_basic_rate * income_basic_subject_to_tax;
-    int income_higher_tax_payable = income_higher_rate * income_higher_subject_to_tax;
-    int income_above_tax_payable = income_above_rate * income_above_subject_to_tax;
+    double income_allowances_tax_payable = income_allowances_rate * income_allowances_subject_to_tax;
+    double income_basic_tax_payable = income_basic_rate * income_basic_subject_to_tax;
+    double income_higher_tax_payable = income_higher_rate * income_higher_subject_to_tax;
+    double income_above_tax_payable = income_above_rate * income_above_subject_to_tax;
 
-    int income_total_payable = income_allowances_tax_payable + income_basic_tax_payable + income_higher_tax_payable + income_above_tax_payable;
+    double income_total_payable = income_allowances_tax_payable + income_basic_tax_payable + income_higher_tax_payable + income_above_tax_payable;
 
     // National Insurance Tax Band
 
@@ -54,27 +54,26 @@ int main(int argc, char const *argv[])
     double national_insurance_upper_earnings_limit_rate = 0.1325;
     double national_insurance_above_rate = 0.0325;
 
-    int national_insurance_primary_threshold_width = 104800;
-    int national_insurance_upper_earnings_limit_width = 418900;
+    double national_insurance_primary_threshold_width = 1048;
+    double national_insurance_upper_earnings_limit_width = 4189;
 
-    int national_insurance_primary_threshold_subject_to_tax = min(national_insurance_primary_threshold_width, monthly_salary);
-    int national_insurance_upper_earnings_limit_subject_to_tax = min(national_insurance_upper_earnings_limit_width, (monthly_salary - national_insurance_primary_threshold_subject_to_tax));
-    int national_insurance_above_subject_to_tax = monthly_salary - national_insurance_primary_threshold_subject_to_tax - national_insurance_upper_earnings_limit_subject_to_tax;
+    double national_insurance_primary_threshold_subject_to_tax = min(national_insurance_primary_threshold_width, monthly_salary);
+    double national_insurance_upper_earnings_limit_subject_to_tax = min(national_insurance_upper_earnings_limit_width, (monthly_salary - national_insurance_primary_threshold_subject_to_tax));
+    double national_insurance_above_subject_to_tax = monthly_salary - national_insurance_primary_threshold_subject_to_tax - national_insurance_upper_earnings_limit_subject_to_tax;
 
-    int national_insurance_primary_threshold_tax_payable = national_insurance_primary_threshold_rate * national_insurance_primary_threshold_subject_to_tax;
-    int national_insurance_upper_earnings_limit_tax_payable = national_insurance_upper_earnings_limit_rate * national_insurance_upper_earnings_limit_subject_to_tax;
-    int national_insurance_above_tax_payable = national_insurance_above_rate * national_insurance_above_subject_to_tax;
+    double national_insurance_primary_threshold_tax_payable = national_insurance_primary_threshold_rate * national_insurance_primary_threshold_subject_to_tax;
+    double national_insurance_upper_earnings_limit_tax_payable = national_insurance_upper_earnings_limit_rate * national_insurance_upper_earnings_limit_subject_to_tax;
+    double national_insurance_above_tax_payable = national_insurance_above_rate * national_insurance_above_subject_to_tax;
 
-    int national_insurance_total_payable = national_insurance_primary_threshold_tax_payable + national_insurance_upper_earnings_limit_tax_payable + national_insurance_above_tax_payable;
+    double national_insurance_total_payable = national_insurance_primary_threshold_tax_payable + national_insurance_upper_earnings_limit_tax_payable + national_insurance_above_tax_payable;
 
     // Grand Total
 
-    int grand_total_payable_pence = income_total_payable + national_insurance_total_payable;
-    int grand_total_payable_pounds = grand_total_payable_pence / 100;
+    double grand_total_payable = income_total_payable + national_insurance_total_payable;
 
-    printf("Income Tax: %dp\n", income_total_payable);
-    printf("National Insurance Tax: %dp\n", national_insurance_total_payable);
-    printf("Total: £%d\n", grand_total_payable_pounds);
+    printf("Income Tax: %fp\n", income_total_payable);
+    printf("National Insurance Tax: %fp\n", national_insurance_total_payable);
+    printf("Total: £%f\n", grand_total_payable);
 
     return 0;
 }
